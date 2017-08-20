3db7fc3136091cccd63faa8c8b4de51b85b253f6
shinyflight <shinyflight@gmail.com>
2017-08-20 22:43:19 +0900

    preprocessing and dataset

---
+++ model.py
@@ -0,0 +1,26 @@
+from __future__ import division
+import os
+import time
+import math
+from glob import glob
+import tensorflow as tf
+import numpy as np
+import yaml
+
+with open("SeqGAN.yaml") as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+
+class MD_GAN(object):
+    def __init__(self, sess):
+
+        ############################################################################
+        #  Hyper-parameters
+        ############################################################################
+        os.environ['CUDA_VISIBLE_DEVICES'] = config['GPU']
+        input_dim = config['INPUT_DIM']
+        batch_size = config['BATCH_SIZE']
+        hidden_dim = config['HIDDEN_DIM']
